128b252ca01deef3ba74c6b29688eba1e983ed11
yogurt0617 <20070617TimmyAlan@gmail.com>
2024-10-03 17:06:31 +0800

    first commit

---
+++ 930.js
@@ -0,0 +1,47 @@
+let a=[]
+let btn    = document.getElementById("btn")
+let add    = document.getElementById("add")
+let data   = document.getElementById("data")
+let error  = document.getElementById("error")
+let btnall = document.getElementById("delete")
+
+
+function del(key){
+    a.splice(key,1)
+    adddata()
+    console.log(a)
+}
+function adddata(){
+    data.innerHTML = null
+    for(let i=0;i<a.length;i++){
+        data.innerHTML += `<li>${a[i]} &nbsp; <button onclick="del(${i})" >清除</button> </li>`
+    }
+
+}
+
+btn.addEventListener("click",()=>{
+    error.innerText = ""
+    if(add.value == ""){
+        error.innerText = "不可為空白"
+        return
+    }
+    a.push(add.value)
+    data.innerHTML = null
+    adddata()
+    add.value = ""
+})
+
+btnall.addEventListener("click",()=>{
+    if(a.length == 0){
+        error.innerText = "沒有資料可清除"
+        return
+    }
+    data.innerHTML = null
+    a = [];
+    
+})
+
+
+
+
+
